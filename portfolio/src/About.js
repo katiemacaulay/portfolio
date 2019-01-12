6ff5efe016d0b96f0133f146ade639a5fe06cb63
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-12 13:35:56 -0600

    getting started

---
+++ portfolio/src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class About extends Component {
+    render() {
+      return (
+        <div>
+            Hello!
+        </div>
+      );
+    }
+  }
+  
+  export default About;
